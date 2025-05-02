edb3e93e439bb31ff8b69cce2e4c6fabe9536c0f
thomas_park <thomas_park@foxitsoftware.com>
2025-05-03 02:26:18 +0900

    fix: apply 2-column video layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import FocusSection from "./components/FocusSection";
 import NewsGrid from "./components/NewsGrid";
+import "./App.css"; // 반드시 포함
 
 function App() {
   const [newsData, setNewsData] = useState({});
@@ -18,12 +19,24 @@ function App() {
       <Header />
       <FocusSection />
       <NewsGrid newsData={newsData} />
-    </div>
-<div className="video-grid">
-  <iframe src="https://youtu.be/KCyNIAeVHQA?si=XJdSSaQdl3-eW-ov" />
-  <iframe src="https://youtu.be/fM4BuCgM0CI?si=-_KO3CAj7avbTRHb" />
-</div>
 
+      {/* 유튜브 영상 2열 배치 */}
+      <h2 className="text-xl font-semibold mt-10 mb-4">화제의 인물 (Youtube)</h2>
+      <div className="video-grid">
+        <iframe
+          src="https://www.youtube.com/embed/KCyNIAeVHQA"
+          title="Video 1"
+          allowFullScreen
+          style={{ width: "100%", height: "215px", border: "none" }}
+        />
+        <iframe
+          src="https://www.youtube.com/embed/fM4BuCgM0CI"
+          title="Video 2"
+          allowFullScreen
+          style={{ width: "100%", height: "215px", border: "none" }}
+        />
+      </div>
+    </div>
   );
 }
 
