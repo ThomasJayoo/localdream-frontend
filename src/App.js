efe77edb19edb7ce0215c10aa8c2dc6cc437352a
thomas_park <thomas_park@foxitsoftware.com>
2025-05-15 23:36:21 +0900

    fix: App.js include Youtube and News

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-// src/components/YoutubeSection.js
 import React from "react";
-import "./components/YoutubeSection.css";
+import YoutubeSection from "./components/YoutubeSection";
+import NewsByCategory from "./NewsByCategory";
 
-const videos = [
-  {
-    title: "집중 논단",
-    url: "https://www.youtube.com/embed/KCyNIAeVHQA"
-  },
-  {
-    title: "촛점 인터뷰",
-    url: "https://www.youtube.com/embed/fM4BuCgM0CI"
-  }
-];
-
-export default function YoutubeSection() {
+function App() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-      {videos.map((video, i) => (
-        <div key={i} className="aspect-video">
-          <iframe
-            title={video.title}
-            src={video.url}
-            frameBorder="0"
-            allowFullScreen
-            className="w-full h-full"
-          ></iframe>
-        </div>
-      ))}
+    <div className="bg-gray-100 min-h-screen">
+      <div className="max-w-5xl mx-auto py-8">
+        <YoutubeSection />
+        <NewsByCategory />
+      </div>
     </div>
   );
 }
+
+export default App;
