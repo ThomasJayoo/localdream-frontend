578f8bf39880ee4fecd749f85996b9dade0c471e
thomas_park <thomas_park@foxitsoftware.com>
2025-05-17 00:18:29 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import React from "react";
 import YoutubeSection from "./components/YoutubeSection";
 import NewsByCategory from "./components/NewsByCategory";
 
-function formatKoreanDate(dateObj) {
-  const week = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
-  const year = dateObj.getFullYear();
-  const month = dateObj.getMonth() + 1;
-  const date = dateObj.getDate();
-  const day = week[dateObj.getDay()];
-  return `${year}년 ${month}월 ${date}일 ${day}`;
+
 
 function App() {
   return (
