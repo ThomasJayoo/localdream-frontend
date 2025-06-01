a41db3557de15db7829279552bc773b1c74c3024
thomas_park <thomas_park@foxitsoftware.com>
2025-06-01 16:42:07 +0900

    🐛 Fix missing useState/useEffect import in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import YoutubeSection from "./components/YoutubeSection";
 import NewsByCategory from "./components/NewsByCategory";
 
